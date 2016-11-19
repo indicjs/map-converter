aeda4a05774ad4f64127828e12741fb556c4c986
Akshay S Dinesh <asdofindia@gmail.com>
2016-11-19 21:27:54 +0530

    add an executable for command line

---
+++ bin/convert.js
@@ -0,0 +1,11 @@
+var System = require('jspm');
+
+System.import('lib/converter').then(function(converter) {
+  if (2 in process.argv) {
+    var infile = process.argv[2];
+  } else {
+    console.log("Make sure you give the filename to convert as a command line argument");
+  }
+  var map = converter.getJSONMap(infile);
+  console.dir(map);
+});
